642f0377812e3e65973f69d9f70c69da00469649
santoshki <santoshkdevp@gmail.com>
2024-08-17 17:37:29 +0530

    Following changes applied: 1. Added routes for handling new ticket creation form data in app.py file. 2. Modified new_ticket_page.html file for form action.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -18,6 +18,27 @@ def handle_buttons():
         return render_template('new_ticket_page.html')
     else:
         return "No button was pressed!"
+
+
+@app.route('/new_create_ticket', methods=['POST'])
+def new_create_ticket():
+    if "submit" in request.form:
+        ticket_title = request.form.get("ticket_title")
+        ticket_description = request.form.get("ticket_description")
+        ticket_priority = request.form.get("ticket_priority")
+        ticket_assignment_group = request.form.get("ticket_assignment_group")
+        ticket_creation_date = request.form.get("ticket_creation_date")
+        ticket_created_by = request.form.get("ticket_created_by")
+
+        print("Ticket title:", ticket_title)
+        print("Ticket Description:", ticket_description)
+        print("Ticket priority:", ticket_priority)
+        print("Ticket Assignment group:", ticket_assignment_group)
+        print("Ticket Creation Date:", ticket_creation_date)
+        print("Ticket created by:", ticket_created_by)
+        return redirect(url_for('home'))
+    return render_template('new_ticket_page.html')
+
 
 
 if __name__ == '__main__':
